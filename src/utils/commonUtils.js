668788e648eb6d4afb2d9d396f90450b3681b430
roshan <roshanmuhmdnavas@gmail.com>
2025-08-08 22:44:07 +0530

    minor fixes downgrade

--- src/utils/commonUtils.js
+++ src/utils/commonUtils.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL, 
+    baseURL: import.meta.env.REACT_APP_API_URL, 
   headers: {
     'Content-Type': 'application/json',
   }
 })
 
 export const addData = async(data,module) => {
-  const addDataRes = await axiosInstance.post(`${process.env.REACT_APP_API_URL}${module}`,data)
+  const addDataRes = await axiosInstance.post(`${import.meta.env.REACT_APP_API_URL}${module}`,data)
 
   if(addDataRes?.data){
     return addDataRes?.data
